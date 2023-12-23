c8756ac8692beb4b7199f35af29ae3835ae950f4
ITurres <emanueliturres.co@gmail.com>
2023-12-22 22:06:53 -0300

    Chore: update 'development.rb' action mailer value

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'my-recipes-246o.onrender.com', port: 443, protocol: 'https' }
 end
